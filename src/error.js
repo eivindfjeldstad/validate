ca43c231762f351fbb80d04abe6d83dbca736ee6
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2018-04-20 18:53:09 +0200

    capture stack trace

--- src/error.js
+++ src/error.js
@@ -7,11 +7,16 @@
 export default class ValidationError extends Error {
   constructor(message, path) {
     super(message);
+
     Object.defineProperty(this, 'path', {
       enumerable: false,
       configurable: true,
       writable: true,
       value: path
     });
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
   }
 }

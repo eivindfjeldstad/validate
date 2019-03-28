4084a032b828df2aec0983f7e279bede11243993
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2019-03-28 15:28:39 +0100

    Add status and expose properties to error

--- src/error.js
+++ src/error.js
@@ -8,15 +8,21 @@ export default class ValidationError extends Error {
   constructor(message, path) {
     super(message);
 
-    Object.defineProperty(this, 'path', {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: path
-    });
+    defineProp(this, 'path', path);
+    defineProp(this, 'expose', true);
+    defineProp(this, 'status', 400);
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, ValidationError);
     }
   }
 }
+
+const defineProp = (obj, prop, val) => {
+  Object.defineProperty(obj, prop, {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: val
+  });
+};
